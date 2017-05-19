02e810d83f4619a471329a6305560342f59147d2
Rich Newman <rich-newman@users.noreply.github.com>
2017-05-19 18:22:12 +0100

    Add tests for fixing

--- src/WebLinterTest/TslintTest.cs
+++ src/WebLinterTest/TslintTest.cs
@@ -3,11 +3,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebLinter;
+using System.IO;
 
 namespace WebLinterTest
 {
     [TestClass]
-    public class TshintTest
+    public class TsLintTest
     {
         [TestMethod, TestCategory("TSLint")]
         public async Task StandardTs()
@@ -17,6 +18,27 @@ namespace WebLinterTest
             Assert.IsFalse(string.IsNullOrEmpty(result.First().Errors.First().FileName), "File name is empty");
             Assert.AreEqual(13, result.First().Errors.Count);
             Assert.AreEqual("if statements must be braced", result.First().Errors.First().Message);
+        }
+
+        [TestMethod, TestCategory("TSLint")]
+        public async Task StandardTsFixErrors()
+        {
+            try
+            {
+                File.Copy("../../artifacts/tslint/a.ts", "../../artifacts/tslint/aTest.ts", true);
+                var result = await LinterFactory.LintAsync(Settings.Instance, true, "../../artifacts/tslint/aTest.ts");
+                Assert.IsTrue(result.First().HasErrors);
+                Assert.IsFalse(string.IsNullOrEmpty(result.First().Errors.First().FileName), "File name is empty");
+                Assert.AreEqual(4, result.First().Errors.Count);
+                Assert.AreEqual("if statements must be braced", result.First().Errors.First().Message);
+                string actual = File.ReadAllText("../../artifacts/tslint/aTest.ts");
+                string expected = File.ReadAllText("../../artifacts/tslint/aFixed.ts");
+                Assert.AreEqual(expected, actual);
+            }
+            finally
+            {
+                File.Delete("../../artifacts/tslint/aTest.ts");
+            }
         }
 
         [TestMethod, TestCategory("TSLint")]
@@ -39,6 +61,27 @@ namespace WebLinterTest
         }
 
         [TestMethod, TestCategory("TSLint")]
+        public async Task StandardTsxFixErrors()
+        {
+            try
+            {
+                File.Copy("../../artifacts/tslint/c.tsx", "../../artifacts/tslint/cTest.tsx", true);
+                var result = await LinterFactory.LintAsync(Settings.Instance, true, "../../artifacts/tslint/cTest.tsx");
+                Assert.IsTrue(result.First().HasErrors);
+                Assert.IsFalse(string.IsNullOrEmpty(result.First().Errors.First().FileName), "File name is empty");
+                Assert.AreEqual(1, result.First().Errors.Count);
+                Assert.AreEqual("The class method 'sayHello' must be marked either 'private', 'public', or 'protected'", result.First().Errors.First().Message);
+                string actual = File.ReadAllText("../../artifacts/tslint/cTest.tsx");
+                string expected = File.ReadAllText("../../artifacts/tslint/cFixed.tsx");
+                Assert.AreEqual(expected, actual);
+            }
+            finally
+            {
+                File.Delete("../../artifacts/tslint/cTest.tsx");
+            }
+}
+
+[TestMethod, TestCategory("TSLint")]
         public async Task StandardTsxNoErrors()
         {
             var result = await LinterFactory.LintAsync(Settings.Instance, "../../artifacts/tslint/d.tsx");
---
+++ src/WebLinterTest/artifacts/tslint/aFixed.ts
@@ -0,0 +1,11 @@
+﻿function foo() {
+    const i = "test";
+    const b = "hat";
+
+    if (i == b)
+        return false;
+    else
+        return "Hello from foo";
+}
+
+console.log(foo());
