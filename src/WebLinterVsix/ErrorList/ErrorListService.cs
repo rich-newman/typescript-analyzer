47a32c65854e4eb4e1abf527f4db619a7bf817e9
Rich Newman <rich-newman@users.noreply.github.com>
2017-05-19 21:49:52 +0100

    Remove redundant comments only

--- src/WebLinterVsix/ErrorList/ErrorListService.cs
+++ src/WebLinterVsix/ErrorList/ErrorListService.cs
@@ -9,8 +9,6 @@ namespace WebLinterVsix
         public static void ProcessLintingResults(IEnumerable<LintingResult> results, string[] fileNames, bool showErrorList)
         {
             IEnumerable<LintingError> errors = results.Where(r => r.HasErrors).SelectMany(r => r.Errors);
-            // This is wrong since it assumes that results contain empty entries for files that were linted but had no errors
-            //IEnumerable<string> clean = results.Where(r => !r.HasErrors).SelectMany(r => r.FileNames);
             IEnumerable<string> clean = fileNames.Where(f => !errors.Select(e => e.FileName).Contains(f));
 
             if (errors.Any())
