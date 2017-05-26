e853794f8b5ad11530c50e60083232d9e2dcdb5c
Rich Newman <rich-newman@users.noreply.github.com>
2017-05-26 22:43:01 +0100

    Unzip edge stuff

--- src/WebLinter/Node/prepare.cmd
+++ src/WebLinter/Node/prepare.cmd
@@ -1,9 +1,9 @@
 REM Modifications Copyright Rich Newman 2017
-7z.exe x -y node.7z
-7z.exe x -y node_modules.7z
+7z.exe x -y edge.7z
+7z.exe x -y -oedge node_modules.7z
 
 del /q 7z.dll
 del /q 7z.exe
-del /q node.7z
+del /q edge.7z
 del /q node_modules.7z
 del /q prepare.cmd
