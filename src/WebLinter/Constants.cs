d7b2087ba6824eefd193bf170639edc30b51fad5
Rich Newman <rich-newman@users.noreply.github.com>
2017-06-19 10:46:31 +0100

    Create version 1.41 for testing issue #7

--- src/WebLinter/Constants.cs
+++ src/WebLinter/Constants.cs
@@ -3,7 +3,7 @@ namespace WebLinter
 {
     public static class Constants
     {
-        public const string VERSION = "1.3";
+        public const string VERSION = "1.41";
         public const string CACHE_NAME = "TypeScriptAnalyzer";
     }
 }
