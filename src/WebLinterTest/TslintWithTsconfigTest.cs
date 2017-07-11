069b646aab028b08f370c788e17ab2d1ab1ccd4a
Rich Newman <rich-newman@users.noreply.github.com>
2017-07-11 21:27:34 +0100

    Add another test

--- src/WebLinterTest/TslintWithTsconfigTest.cs
+++ src/WebLinterTest/TslintWithTsconfigTest.cs
@@ -98,6 +98,50 @@ namespace WebLinterTest
             Assert.AreEqual(0, completedDocsError.ColumnNumber);
         }
 
+        [TestMethod, TestCategory("TSLint with tsconfig")]
+        public async Task LintAll()
+        {
+            // Arrange
+            string topTsconfig = Path.GetFullPath(@"../../artifacts/tsconfig/multiple/tsconfig.json");
+            string folderaTsconfig = Path.GetFullPath(@"../../artifacts/tsconfig/multiple/a/tsconfig.json");
+            string folderbTsconfig = Path.GetFullPath(@"../../artifacts/tsconfig/multiple/b/tsconfig.json");
+            // The tsconfig.json below isn't in a VS project, so couldn't be linted from the UI - can be here
+            string folderbTsconfigEmptyProject = Path.GetFullPath(@"../../artifacts/tsconfig/none/b/tsconfig.json");
+            string[] fileNames = new string[] { topTsconfig, folderaTsconfig, folderbTsconfig, folderbTsconfigEmptyProject };
+
+            // Act
+            var result = await LinterFactory.Lint(new Settings() { TSLintUseTSConfig = true }, false, false, fileNames);
+
+            // Assert
+            // file2 is in both tsconfigs.  It has 4 errors.  With the old code we got 8 in the Error List, and here.
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(22, result[0].Errors.Count);
+
+            IList<LintingError> file1Errors = GetErrorsForFile("file1.ts", result[0].Errors);
+            Assert.IsTrue(file1Errors.Count == 3);
+
+            IList<LintingError> file2Errors = GetErrorsForFile("file2.ts", result[0].Errors);
+            Assert.IsTrue(file2Errors.Count == 4);
+            LintingError completedDocsError = file2Errors.First(le => le.ErrorCode == "completed-docs");
+            Assert.IsNotNull(completedDocsError);
+            Assert.AreEqual(2, completedDocsError.LineNumber);
+            Assert.AreEqual(0, completedDocsError.ColumnNumber);
+
+            IList<LintingError> file3Errors = GetErrorsForFile("file3.ts", result[0].Errors);
+            Assert.IsTrue(file3Errors.Count == 3);
+
+            IList<LintingError> file4Errors = GetErrorsForFile("file4.ts", result[0].Errors);
+            Assert.IsTrue(file4Errors.Count == 3);
+
+            IList<LintingError> file5Errors = GetErrorsForFile("file5.ts", result[0].Errors);
+            Assert.IsTrue(file5Errors.Count == 4);
+
+            IList<LintingError> testErrors = GetErrorsForFile("test.ts", result[0].Errors);
+            Assert.IsTrue(testErrors.Count == 5);
+
+        }
+
         private IList<LintingError> GetErrorsForFile(string fileName, IEnumerable<LintingError> allErrors)
         {
             List<LintingError> result = new List<LintingError>();
