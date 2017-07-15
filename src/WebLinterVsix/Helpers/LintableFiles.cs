66af2a17f537b84ef987d109aa85557944641a17
Rich Newman <rich-newman@users.noreply.github.com>
2017-07-15 17:25:23 +0100

    Deal with the edge case where we rightclick a ts file with use tsconfig set and the ts file is on the ignore path: for consistency it should be possible to run the linter, find a tsconfig not on the ignore path, and lint.  This because it will lint if we run for the solution in this case.  Phew.

--- src/WebLinterVsix/Helpers/LintableFiles.cs
+++ src/WebLinterVsix/Helpers/LintableFiles.cs
@@ -1,5 +1,6 @@
 ﻿using EnvDTE;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using WebLinter;
@@ -17,7 +18,12 @@ namespace WebLinterVsix.Helpers
         public static bool AreAllSelectedItemsLintable()
         {
             UIHierarchyItem[] selectedItems = WebLinterPackage.Dte.ToolWindows.SolutionExplorer.SelectedItems as UIHierarchyItem[];
-
+            // In this test we check whether what's clicked on is lintable, meaning it's an item that might contain
+            // files that can be linted.  We don't actually check whether there ARE any files that can be linted associated
+            // with the item.  For example, if you rightclick a solution file we check it's a solution file, but we don't 
+            // check for valid .ts or .tsx files in the solution. This applies to the ignore options as well (ignore patterns, 
+            // ignore nested).
+            // TODO: make the right-click check if lintable files associated with the clicked item exist
             foreach (UIHierarchyItem selectedItem in selectedItems)
             {
                 if (!IsLintable(selectedItem)) return false;
@@ -29,7 +35,6 @@ namespace WebLinterVsix.Helpers
         {
             return selectedItem.Object is Solution ||
                    selectedItem.Object is Project ||
-                   selectedItem.Object is EnvDTE80.SolutionFolder ||
                   (selectedItem.Object is ProjectItem item &&
                         item.GetFullPath() is string projectItemPath &&
                         IsLintableProjectItem(projectItemPath));
@@ -39,8 +44,10 @@ namespace WebLinterVsix.Helpers
         public static bool IsLintableProjectItem(string projectItemPath)
         {
             return IsLintableDirectory(projectItemPath) ||
-                   IsLintableTsOrTsxFile(projectItemPath) ||
-                   (WebLinterPackage.Settings.UseTsConfig && IsLintableTsconfig(projectItemPath));
+                   (!WebLinterPackage.Settings.UseTsConfig && IsLintableTsOrTsxFile(projectItemPath)) ||
+                   (WebLinterPackage.Settings.UseTsConfig &&
+                       (IsLintableTsconfig(projectItemPath) ||
+                        IsLintableTsOrTsxFile(projectItemPath, checkIgnoreOptions: false)));
         }
 
         public static bool IsLintableDirectory(string path)
@@ -52,12 +59,12 @@ namespace WebLinterVsix.Helpers
             return true;
         }
 
-        public static bool IsLintableTsOrTsxFile(string fileName)
+        public static bool IsLintableTsOrTsxFile(string fileName, bool checkIgnoreOptions = true)
         {
             // Check if filename is absolute because when debugging, script files are sometimes dynamically created.
             if (string.IsNullOrEmpty(fileName) || !Path.IsPathRooted(fileName)) return false;
             if (!LinterFactory.IsExtensionTsOrTsx(fileName)) return false;
-            return IsLintableFile(fileName);
+            return IsLintableFile(fileName, checkIgnoreOptions);
         }
 
         public static bool IsLintableTsconfig(string fileName)
@@ -67,25 +74,28 @@ namespace WebLinterVsix.Helpers
             return IsLintableFile(fileName);
         }
 
-        private static bool IsLintableFile(string fileName)
+        private static bool IsLintableFile(string fileName, bool checkIgnoreOptions = true)
         {
-            if (WebLinterPackage.Settings == null || WebLinterPackage.Settings.GetIgnorePatterns().Any(p => fileName.Contains(p)))
-                return false;
-
             ProjectItem item = WebLinterPackage.Dte.Solution.FindProjectItem(fileName);
             bool isInProject = item?.GetFullPath() is string;
             if (!isInProject) return false;
 
-            // Ignore nested files
-            if (WebLinterPackage.Settings.IgnoreNestedFiles)
+            if (checkIgnoreOptions)
             {
-                // item.Collection is not supported in Node.js projects
-                if (item.ContainingProject.Kind.Equals("{9092aa53-fb77-4645-b42d-1ccca6bd08bd}", StringComparison.OrdinalIgnoreCase))
-                    return true;
-
-                if (item.Collection?.Parent is ProjectItem parent &&
-                    parent.Kind == EnvDTE.Constants.vsProjectItemKindPhysicalFile)
+                if (WebLinterPackage.Settings == null || WebLinterPackage.Settings.GetIgnorePatterns().Any(p => fileName.Contains(p)))
                     return false;
+
+                // Ignore nested files
+                if (WebLinterPackage.Settings.IgnoreNestedFiles)
+                {
+                    // item.Collection is not supported in Node.js projects
+                    if (item.ContainingProject.Kind.Equals("{9092aa53-fb77-4645-b42d-1ccca6bd08bd}", StringComparison.OrdinalIgnoreCase))
+                        return true;
+
+                    if (item.Collection?.Parent is ProjectItem parent &&
+                        parent.Kind == EnvDTE.Constants.vsProjectItemKindPhysicalFile)
+                        return false;
+                }
             }
 
             return true;
