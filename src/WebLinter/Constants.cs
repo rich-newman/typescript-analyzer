3271340eb9e2c5bcdc79de7f456642cebb6d4910
Rich Newman <rich-newman@users.noreply.github.com>
2018-04-19 21:30:56 +0100

    Update version number to 1.15

--- src/WebLinter/Constants.cs
+++ src/WebLinter/Constants.cs
@@ -3,7 +3,7 @@ namespace WebLinter
 {
     public static class Constants
     {
-        public const string VERSION = "1.14";
+        public const string VERSION = "1.15";
         public const string CACHE_NAME = "TypeScriptAnalyzer";
     }
 }
